50ca49ef58e9c314863d80f02760e55d48bd907f
it__zxm <it__zxm@163.com>
2017-07-29 08:25:15 +0800

    w

--- SalaryOfEmployee/PaymentScheduleImpl/BiweeklySchedule.java
+++ SalaryOfEmployee/PaymentScheduleImpl/BiweeklySchedule.java
@@ -6,10 +6,11 @@ import interfaces.PaymentSchedule;
 import ocp.DateUtil;
 
 public class BiweeklySchedule implements PaymentSchedule {
-	Date firstPayableFriday =DateUtil.pareDate("2017-4-3");
+	
 	@Override
 	//firstPayableFriday 第一次支付的周5 
 	public boolean isPayDay(Date date) {
+		Date firstPayableFriday =DateUtil.pareDate("2017-4-3");
 		long interval=DateUtil.getDaysBetween(date,firstPayableFriday);
 		return interval%14==0;
 	}
